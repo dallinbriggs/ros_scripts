f3384dba563e44a883ba1d3278081fc5d3ff76fb
Dallin Briggs <dallinbriggs@gmail.com>
2018-02-09 16:44:13 -0700

    started script logic

--- rosbag_reindex.sh
+++ rosbag_reindex.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-echo Enter the directory containing the bag files. If here, press enter.
+echo "This script reindexes all of the .active rosbags within a directory."
+echo "Are the bags in this directory? [y/n]"
 read DIRECTORY
-if [ -z DIRECTORY ]
-  DIRECTORY=$pwd
+
+if [ "$DIRECTORY" == "n" ]; then
+  echo "Enter the directory containing the bag files."
+  read DIRECTORY
+  echo "TODO 1"
+fi
+
+if [ "$DIRECTORY" == "y" ]; then
+  echo "Reindexing bags in this directory."
+  rosbag reindex *.bag.active
+fi
+
+for []
+
 echo $active_count rosbags out of $total_count need to be reindexed.
 echo Would you like to reindex them all at once?
 echo $
