3ad01c256bf62d65da005c5858bae2c1273905be
dallinbriggs <dallinbriggs@gmail.com>
2018-02-12 15:07:49 -0700

    almost working

--- rosbag_reindex.sh
+++ rosbag_reindex.sh
@@ -3,23 +3,36 @@ echo "This script reindexes all of the .active rosbags within a directory."
 echo "Are the bags in this directory? [y/n]"
 read DIRECTORY
 
+if [ "$DIRECTORY" == "y" ]; then
+  PATH="$PWD"
+  echo "path is $PATH"
+fi
+
 if [ "$DIRECTORY" == "n" ]; then
   echo "Enter the directory containing the bag files."
-  read DIRECTORY
-  cd $DIRECTORY
-  echo $PWD
+  read PATH
 fi
 
-if [ "$DIRECTORY" == "y" ]; then
-  active_count = $"ls -1 *bag.active | wc -l"
+{ cd $PATH } || { echo "Invalid path. Enter the correct directory."
 
-  echo "Found $active_count active rosbags."
-  echo "Reindexing bags in this directory."
-  # rosbag reindex *.bag.active
+active_count="$(ls -1 *bag.active | wc -l)"
+total_count="$(ls -1 *.bag* | wc -l)"
+
+echo "Found $active_count active rosbags out of $total_count rosbags."
+echo "Would you like to reindex them? [y/n]"
+read REINDEX
+
+if [ "$REINDEX" == "y" ]; then
+  echo "Reindexing active rosbags."
+  rosbag reindex *.bag.active
 fi
 
-for []
+wait
 
-echo $active_count rosbags out of $total_count need to be reindexed.
-echo Would you like to reindex them all at once?
-echo $
+echo "Rosbags have been reindexed."
+echo "Would you like to rename the .bag.active files to .bag files? [y/n]"
+read RENAME
+
+if [ "$RENAME" == "y" ]; then
+
+fi
